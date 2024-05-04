1933f08eec2b6d15e77148b42ed680abfc2a7963
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-04 15:14:17 +0200

    switch from moralis

--- processor.py
+++ processor.py
@@ -3,6 +3,7 @@ import aiohttp
 from bs4 import BeautifulSoup
 import lxml
 import os
+import random
 from web3 import AsyncWeb3
 
 from utils.abis import UNISWAP_PAIR, ERC20
@@ -20,25 +21,66 @@ def get_contract_addresses(soup:BeautifulSoup):
     return [{"PAIR_ADDRESS": row['href'].split("/")[-1]} for row in rows]
 
 async def get_paired_token(address):
+    if not address:
+        return
     contract = web3.eth.contract(address=web3.to_checksum_address(address), abi=UNISWAP_PAIR)
-    token_0 =  await contract.functions.token0().call()    
-    token_1 =  await contract.functions.token1().call()
-    if token_0 == "0x4200000000000000000000000000000000000006":
-        return token_1 
-    return token_0
+    # token_0 =  await contract.functions.token0().call()    
+    # token_1 =  await contract.functions.token1().call()
+    # token_data = {}
+    # if token_0 == "0x4200000000000000000000000000000000000006":
+    #     token_data = await get_token_data(token_1)
+    # else:
+    #     token_data = await get_token_data(token_0)
+    liquidity_data = await get_liquidity_events(address)
+    print(liquidity_data)
+    # sync_data = await get_sync_events(contract)
 
 async def get_token_data(address):
+    if not address:
+        return
     contract = web3.eth.contract(address=web3.to_checksum_address(address), abi=ERC20)
     symbol = await contract.functions.symbol().call()
     supply = await contract.functions.totalSupply().call()
     return {"address": address, "symbol": symbol, "supply": int(supply)/10**18}
 
-async def get_pair_events(address):
-    
+async def get_liquidity_events(address):
+    # rewrite using jsonrpc
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "eth_getLogs",
+        "params": [
+            {
+            "fromBlock": "0xd5eb21",
+            "toBlock": "latest",
+            "address": address,
+            "topics": [
+                "0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496"
+            ]
+            }
+        ]
+    }
+
+    print(payload)
+    print(PROVIDER_LINK)
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }   
+    async with aiohttp.ClientSession() as session:
+        async with session.post("https://api.developer.coinbase.com/rpc/v1/base/wd7CKNfSnWyjmYdCaxBUCe8BUM_onk9C", json=payload, headers=headers) as response:
+            data = await response.json()
+            return data
+
+async def get_pair_events(contract):
+    # get sync and liquidity events
+    ...
 
 async def process_token(pair_address):
     token = await get_paired_token(pair_address["PAIR_ADDRESS"])
-    pair_address.update(await get_token_data(token))
+    if token:
+        pair_address.update(await get_token_data(token))
 
 async def process_page(html):
     with open(html) as f:
@@ -46,8 +88,8 @@ async def process_page(html):
     soup = BeautifulSoup(html, 'lxml')
     addresses = get_contract_addresses(soup)
     # speed this up with asyncio.gather
-    for i in range(0, len(addresses), 10):
-        tasks = [process_token(address) for address in addresses[i:i+10]]
+    for i in range(0, len(addresses), 2):
+        tasks = [process_token(address) for address in addresses[i:i+2]]
         await asyncio.gather(*tasks)
         # update address with token data
 
@@ -55,6 +97,6 @@ async def process_page(html):
         #     address["ADDRESS"] = token
         #     address.update(await get_token_data(token))
         break
-    print(addresses[:10])
+    # print(addresses[:10])
 
 asyncio.run(process_page(html))
