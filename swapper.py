17faea271cf0d37e809ffe85b33b03501bf9cf21
jakkie <jakubkielbasiewicz@gmail.com>
2024-05-12 08:59:44 +0200

    swapper init

---
+++ swapper.py
@@ -0,0 +1,10 @@
+
+
+async def get_token_price():
+    ...
+
+async def swap_eth_for_token():
+    # check if eth balance is enough
+    # get token price
+    # calculate min amount of token to receive (1-slippage) * token price
+    
